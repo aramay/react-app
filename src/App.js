4bb1fa03b38bbffdd29020c58ac1d597fe32bfc7
“kasper341” <kasper_341@hotmail.com>
2017-01-05 13:30:54 -0800

    demo children props

--- src/App.js
+++ src/App.js
@@ -1,49 +1,26 @@
-import React from 'react';
+import React from 'react'
 
 class App extends React.Component {
 
-    constructor () {
-        super();
-            this.state = {
-                txt: "this is state txt",
-                cat: 0
-            }
-    }
-
-    update ( e ) {
-        this.setState({txt: e.target.value})
-    }
     render () {
-        // let x = this.props;
-        let x = this.state;
-        console.log(x)
+        return <Button>I<Heart />React</Button>
+    }
+}
+
+const Button = (props) => {
+    let x = props;
+    console.log(x)
+
+    return (
+        <button>{props.children}</button>
+    )
+}
+
+class Heart extends React.Component {
+    render () {
         return (
-                // <input type='text' onChange={this.update.bind(this)}/>   
-                // <h1>{x.txt}</h1>
-                // <h1>{x.cat}</h1>
-            <div>
-                <h1>{this.state.txt} - {this.state.cat}</h1>
-                <Widget update={this.update.bind(this)}/>
-            </div>
+            <span>&hearts;</span>
         )
     }
-
-}
-
-App.propTypes = {
-    txt: React.PropTypes.string,
-    cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-    txt: "this is default text"
-}
-
-const Widget = (props) => {
-    let x = props
-    console.log("props ", x)
-    return (
-        <input type='text' onChange={props.update}/>
-    )
 }
 export default App
