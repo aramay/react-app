2be9f86c762927e66615bf510e55e4ee573a4848
“kasper341” <kasper_341@hotmail.com>
2017-01-05 12:14:39 -0800

    setup project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class App extends React.Component {
+
+    render () {
+        return (
+            <div>   
+                <h1>hello world</h1>
+                <h1>hello world</h1>
+            </div>
+        )
+    }
+}
+
+export default App
