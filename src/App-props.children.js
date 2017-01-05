54739f4412f23f5219b2aeec1b2b1ad4f08916f4
“kasper341” <kasper_341@hotmail.com>
2017-01-05 13:44:57 -0800

    move to 'props.children'

---
+++ src/App-props.children.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+class App extends React.Component {
+
+    render () {
+        return <Button>I<Heart />React</Button>
+    }
+}
+
+const Button = (props) => {
+    let x = props;
+    console.log(x)
+
+    return (
+        <button>{props.children}</button>
+    )
+}
+
+class Heart extends React.Component {
+    render () {
+        return (
+            <span>&hearts;</span>
+        )
+    }
+}
+export default App
