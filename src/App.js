3ba32a9273cbc0464d60626f14bb19f50aa08cfb
“kasper341” <kasper_341@hotmail.com>
2017-01-05 12:35:27 -0800

    add 'props' and 'props validation'

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import React from 'react';
 class App extends React.Component {
 
     render () {
+        let x = this.props;
+        console.log(x)
         return (
             <div>   
-                <h1>hello world</h1>
-                <h1>hello world</h1>
+                <h1>{x.txt}</h1>
+                <h1>{x.cat}</h1>
+                
             </div>
         )
     }
+
 }
 
+App.propTypes = {
+    txt: React.PropTypes.string,
+    cat: React.PropTypes.number.isRequired
+}
+
+App.defaultProps = {
+    txt: "this is default text"
+}
 export default App
