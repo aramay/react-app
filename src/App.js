9d303172dc9f0eaf6d24b9edd61b8773c3fea98e
“kasper341” <kasper_341@hotmail.com>
2017-01-08 17:40:13 -0800

    implement filter for list of data

--- src/App.js
+++ src/App.js
@@ -1,45 +1,61 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+// var axios = require('axios')
+import axios from 'axios'
 
 class App extends React.Component {
-    constructor () {
+    constructor() {
         super();
-        this.state = {increasing: false}
+        this.state = {items: []}
     }
-    update () {
-        ReactDOM.render(
-            <App val={this.props.val + 1}/>,
-            document.getElementById('root')
-        )
-    }
-    componentWillReceiveProps(nextProps) {
-        console.log("componentWillReceiveProps nextProps", nextProps)
-        this.setState({increasing: nextProps.val > this.props.val})
-    }
-    //does not prevent props from being updated
-    //it only prevents a re-render
-    shouldComponentUpdate(nextProps, nextState) {
-        console.log("nextProps ", nextProps)
-        console.log("nextState ", nextState)
+
+    componentWillMount() {
+
+        let items = []
+
+        axios.get(`http://swapi.co/api/people/?format=json`)
+            .then(res => {
         
-        return nextProps.val % 5 === 0
+            items = res.data.results.map(obj => obj.name);
+            this.setState({items: items})
+
+        }).catch(function(error){
+            console.log("Get request failed ", error)
+        })
+    }
+
+    filter(e){
+        console.log("filter", e.target.value)
+        this.setState({filter: e.target.value})
     }
     render () {
-        // console.log(this)
-        console.log("increasing in render", this.state.increasing)
+        let items = this.state.items
+        
+        if(this.state.filter){
+            items = items.filter( item => {
+                return item.includes(this.state.filter)
+            })
+            // console.log("filtered items ", items)
+            // return items
+        }
+        
+        console.log("render ", this.state)
+
         return (
             <div>
-                <h1>hello</h1>
-                <button onClick={this.update.bind(this)}>Add </button>
-                <p>{this.props.val}</p>
+                <h1>Names</h1>
+                <input type='text'
+                    onChange={this.filter.bind(this)} >
+                </input>
+
+                <div>
+                {items.map(item => 
+                    <p>{item}</p>
+                )}
+                </div>
+
             </div>
         )
     }
-    componentDidUpdate(prevProps, prevState) {
-        console.log("prevProps ", prevProps)
-    }
 }
-
-App.defaultProps = {val: 0}
 
 export default App
